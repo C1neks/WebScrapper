fd0ae15f6e032afb4dd05b131d137387c104bff7
C1neks <41152949+C1neks@users.noreply.github.com>
2021-09-01 13:44:42 +0200

    PrintingTitles

---
+++ app.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+
+request('https://www.olx.pl/nieruchomosci/dzialki/sprzedaz/sejny/?search%5Bdist%5D=10)',
+    (error,response, html) => {
+    if(!error && response.statusCode ==200) {
+        const $ = cheerio.load(html);
+        const postTitles = [];
+
+        $('h3 > a > strong').each((i, el) => {
+            const postTitle = $(el).text()
+            postTitles.push(postTitle)
+
+        });
+        console.log(postTitles);
+
+
+    }
+});
+
