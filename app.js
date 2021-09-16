520fca6e45311f0c54cd03109423b789877d1a9d
C1neks <baranowskimarcin7@gmail.com>
2021-09-16 11:55:57 +0200

    new

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 const {default: axios} = require('axios');
 const {getHtml} = require('./app2');
 const {html} = require('cheerio/lib/api/manipulation');
-const {title} = require('process');
-const links = [];
-const titles = [];
 const baseUrl = 'https://www.olx.pl/nieruchomosci/dzialki/sprzedaz/sejny/?search%5Bdist%5D=10)';
 
-const Scrapper = () => {
+const Scrapper = async () => {
+
+    let links = [];
+    let titles = [];
+    let prices = [];
+    let currencies = [];
+    let imgs = [];
+    let descriptions = [];
+    let areas = [];
+    let units = [];
 
     let data;
     try {
@@ -26,7 +32,8 @@ const Scrapper = () => {
         price: '.price > strong',
         title: '[data-cy="ad_title"]',
         description: '[data-cy="ad_description"] div',
-        area: 'li:nth-last-of-type(2) > p'
+        area: 'li:nth-last-of-type(2) > p',
+        unit: 'li:nth-last-of-type(1) > p'
     }
 
     const saveData = (data, file) => {
@@ -42,24 +49,32 @@ const Scrapper = () => {
         console.log('saved')
     }
 
-    class Post {
-        constructor(title, link, img, price) {
-            this.title = title;
-            this.link = link;
-            this.img = img;
-            this.price = price;
+    // class Post {
+    //     constructor(link,title,price ,currency, img, description, area) {
+    //         this.title = title;
+    //         this.link = link;
+    //         this.img = img;
+    //         this.price = price;
+    //         this.currency = currency;
+    //         this.description = description;
+    //         this.area = area;
+    //     }
+    // }
+
+    // const saveOffer = async (title, link, img, price) => {
+    //
+    //     let newOffer = await new Post(title, link, img, price)
+    //     data[newOffer.link] = newOffer
+    //     saveData(data, 'results.json')
+    // }
+        const saveOffer = async (result) => {
+            result.forEach(index => {
+                data[result.link] = result
+                saveData(data,'results.json')
+            })
         }
-    }
 
-    const saveOffer = async (title, link, img, price) => {
-
-        let newOffer = await new Post(title, link, img, price)
-        data[newOffer.link] = newOffer
-        saveData(data, 'results.json')
-    }
-
-
-    const getData = async () => {
+    const getOfferLinks = async () => {
 
         const html = await getHtml(baseUrl);
         const $ = cheerio.load(html);
@@ -68,63 +83,90 @@ const Scrapper = () => {
             const link = $(el)
                 .find(SELECTORS.link)
                 .attr('href')
-            links.push(link)
-            const head = $(el)
-                .find(SELECTORS.head)
-                .text();
-            //  console.log(link)
-            const img = $(el)
-                .find(SELECTORS.img)
-                .attr('src');
-            const priceInString = $(el)
-                .find(SELECTORS.price)
-                .text()
-                .replace(/ /g, '');
-            let price = parseInt(priceInString);
-            // console.log(head,link,img,price);
-            saveOffer(head, link, img, price)
+            links = [...links, link]
         })
         return links
     }
 
     const getTitle = async () => {
-        const urls = await getData()
+        const urls = await getOfferLinks()
 
-        urls.forEach((index) => {
+        for (const index of urls) {
 
-            const html = getHtml(index);
+            const html = await getHtml(index);
             const $ = cheerio.load(html);
             console.log(index)
             $(SELECTORS.title).each((i, el) => {
                 const title = $(el)
-                    .text();
-                titles.push(title)
-                console.log(title)
+                    .text()
+                titles = [...titles, title]
+
             });
 
+            $('[data-testid=ad-price-container] > h3').each((i, el) => {
+                const priceInString = $(el)
+                    .text()
+                    .replace(/ /g, '');
+                let price = parseInt(priceInString)
+                let currency = priceInString.slice(-2)
+                prices = [...prices,price]
+                currencies = [...currencies,currency]
+                // console.log(price);
+                // console.log(currency);
+            })
+            $('.swiper-zoom-container').each((i, el) => {
+                const img = $(el)
+                    .find(SELECTORS.img)
+                    .attr('src')
+                imgs = [...imgs,img]
+                // console.log(img)
+            })
             $(SELECTORS.description).each((i, el) => {
                 const description = $(el)
                     .text()
-                console.log(description)
+                descriptions = [...descriptions,description]
+                // console.log(description)
             })
 
             $('.css-sfcl1s').each((i, el) => {
                 let areaInString = $(el)
                     .find(SELECTORS.area)
                     .text()
-                    .replace(/ /g, '');
+                    .replace(/\D/g, "");
                 let area = parseInt(areaInString)
-                console.log(area);
+                areas = [...areas,area]
+                // console.log(areaInString);
+                let priceForMeter = $(el)
+                    .find(SELECTORS.unit)
+                    .text()
+                let unit = priceForMeter.slice(-2)
+                units = [...units,unit]
 
             })
 
-        })
 
-        console.log(urls)
-        return titles
+        }
+        // console.log(titles,prices,currencies,imgs,descriptions,areas)
+
+        // console.log(urls)
+        // return titles
     }
-    getTitle()
-    // getData()
+    await getTitle()
+    getOfferLinks()
+    let result = links.map((id,index) => {
+        return {
+            link: id,
+            title: titles[index],
+            price: prices[index],
+            currency: currencies[index],
+            img: imgs[index],
+            description: descriptions[index],
+            area: areas[index],
+            unit: units[index]
+
+        }
+    });
+    await saveOffer(result)
 
 }
 
